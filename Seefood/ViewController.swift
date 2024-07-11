38ed150602651944af6cdf6f3cb06eeb7a089d4f
Prajwal2212 <bt22csa043@iiitn.ac.in>
2024-07-11 15:31:56 +0530

    Initial Commit

---
+++ Seefood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seefood
+//
+//  Created by Prajwal Prasad on 11/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
